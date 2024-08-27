e8950878e4f05afb68594362f6eadec9692ea258
USRVI-LC4 <USRVI-LC4@VIAWSE02LC0400.duoc.acad>
2024-08-26 21:54:56 -0400

    condiciones

---
+++ DSY1102-3V/UsoCondiciones/src/Condiciones.java
@@ -0,0 +1,45 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author Profe Hernán
+ * @version 26-08-2024
+ */
+public class Condiciones {
+    
+    private int edad;
+    private String nombre;
+
+    public Condiciones() {
+    }
+
+    public Condiciones(int edad, String nombre) {
+        this.edad = edad;
+        this.nombre = nombre;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    
+    
+    
+    
+    
+}
